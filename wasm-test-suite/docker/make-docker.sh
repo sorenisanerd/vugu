0e2cfda0467593ed52bf3ca40a463524d021e64c
Søren L. Hansen <sorenisanerd@gmail.com>
2022-01-30 12:07:00 -0800

    Parameterize docker image id

--- wasm-test-suite/docker/make-docker.sh
+++ wasm-test-suite/docker/make-docker.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+IMAGE=${IMAGE:-vugu/wasm-test-suite\:latest}
+
 # Go build for linux
 GOOS=linux GOARCH=amd64 go build -o wasm-test-suite-srv .
 
 # Docker build and tag image
-docker build -t vugu/wasm-test-suite:latest .
+docker build -t "${IMAGE}" .
 
-echo "Run with: docker run -ti --rm -p 9222:9222 -p 8846:8846 vugu/wasm-test-suite:latest"
-echo "To push to DockerHub, run 'docker login' and then 'docker push vugu/wasm-test-suite:latest'"
+echo "Run with: docker run -ti --rm -p 9222:9222 -p 8846:8846 ${IMAGE}"
+echo "To push to DockerHub, run 'docker login' and then 'docker push ${IMAGE}'"
